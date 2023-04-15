68df8d8ff6bfa343bf1499e4306834ec39151f1d
codezeros18 <125339231+codezeros18@users.noreply.github.com>
2023-04-15 21:57:13 +0700

    about-page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,92 @@
 @extends('layout.main')
 
 @section('content')
-    <h1>About</h1>
+    <h2 class="mx-auto mb-4 text-center" style="margin-top: 70px">TENTANG KAMI </h2>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="card mb-3 my-5" style="max-width: 100%;">
+                    <div class="row g-0 my-5">
+                      <div class="col-md-4">
+                        <img src="../img/brand-logo-new-2.png" style="weight:200px; height:200px; border-radius:100px" class="mx-auto d-block img-fluid" alt="...">
+                      </div>
+                      <div class="col-md-8 my-auto ">
+                        <div class="card-body">
+                          <h5 class="card-title">A Real-time Auction Site for Everyone.</h5>
+                          <p class="card-text">Pojok Lelang - website lelang online yang ideal bagi
+                            semua orang untuk menemukan barang-barang
+                            impian mereka dengan mudah, aman, dan efisien.</p>
+                          <p class="card-text"><small class="text-body-secondary">Motto Kami: Everyone Can Bid.</small></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+            </div>
+        </div>
+        <div class="row mt-4 mb-5 row-cols-1 row-cols-sm-2 g-5 ">
+
+            <div class="col">
+                <div class="text-center mb-5">
+                    <h5>HUBUNGI KAMI</h5>
+                </div>
+
+                <div class="row row-cols-1 row-cols-sm-2 g-4">
+                  <div class="col d-flex flex-column gap-2">
+                    <div class="d-inline-flex align-items-center justify-content-center fs-4">
+                        <p style="font-size: 18px" class="text-body-secondary"><i class="bi bi-whatsapp"></i> +62-812-8481-8862</p>
+                    </div>
+                  </div>
+
+                  <div class="col d-flex flex-column gap-2">
+                    <div class="d-inline-flex align-items-center justify-content-center fs-4">
+                        <p style="font-size: 18px" class="text-body-secondary"><i class="bi bi-whatsapp"></i> +62-812-8481-8862</p>
+                    </div>
+                  </div>
+
+                  <div class="col d-flex flex-column gap-2">
+                    <div class="d-inline-flex align-items-center justify-content-center fs-4">
+                        <p style="font-size: 18px" class="text-body-secondary"><i class="bi bi-whatsapp"></i> +62-812-8481-8862</p>
+                    </div>
+                  </div>
+
+                  <div class="col d-flex flex-column gap-2">
+                    <div class="d-inline-flex align-items-center justify-content-center fs-4">
+                        <p style="font-size: 18px" class="text-body-secondary"><i class="bi bi-whatsapp"></i> +62-812-8481-8862</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            <div class="col">
+                <div class="text-center mb-5">
+                    <h5>IKUTI KAMI</h5>
+                </div>
+
+                <div class="row row-cols-1 row-cols-sm-2 g-4">
+                  <div class="col d-flex flex-column gap-2">
+                    <div class="d-inline-flex align-items-center justify-content-center fs-4">
+                        <p style="font-size: 18px" class="text-body-secondary"><i class="bi bi-instagram"></i> PojokLelang</p>
+                    </div>
+                  </div>
+
+                  <div class="col d-flex flex-column gap-2">
+                    <div class="d-inline-flex align-items-center justify-content-center fs-4">
+                        <p style="font-size: 18px" class="text-body-secondary"><i class="bi bi-github"></i> PojokLelang</p>
+                    </div>
+                  </div>
+
+                  <div class="col d-flex flex-column gap-2">
+                    <div class="d-inline-flex align-items-center justify-content-center fs-4">
+                        <p style="font-size: 18px" class="text-body-secondary"><i class="bi bi-twitter"></i> PojokLelang</p>
+                    </div>
+                  </div>
+
+                  <div class="col d-flex flex-column gap-2">
+                    <div class="d-inline-flex align-items-center justify-content-center fs-4">
+                        <p style="font-size: 18px" class="text-body-secondary"><i class="bi bi-facebook"></i> PojokLelang</p>
+                    </div>
+                  </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
