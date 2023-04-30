de20fa8455dc15f58ad691a06457e5518ed0ae65
r-aozora <zytrahidayat11@gmail.com>
2023-04-30 12:10:31 +0700

    redesign UI pt 3 - profile & about page

--- resources/views/part/link.blade.php
+++ resources/views/part/link.blade.php
@@ -1,5 +1,6 @@
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
 <link href="img/brand-logo-new-2.png" rel="icon">
 <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
