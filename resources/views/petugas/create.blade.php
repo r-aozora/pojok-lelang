2fc9f9d646f60ca358f7e174d3c6b5c45e7cdd4e
r-aozora <zytrahidayat11@gmail.com>
2023-05-01 15:29:32 +0700

    error in redesign

--- resources/views/petugas/create.blade.php
+++ resources/views/petugas/create.blade.php
@@ -1,57 +1,61 @@
 @extends('layout.main')
 
 @section('content')
-    <div class="container mb-4">
-        <h2 class="mt-3">Tambah Data Petugas</h2>
-        <div class="row">
-            <div class="col">
-                <div class="card mt-3 mb-3">
-                    <div class="card-body">
-                        {{-- <div class="card-header mb-3" style="background-color:#055E68; max-height:60px">
-                            <h5 class="card-title mt-2 fw-medium text-light">{{ $petugas->nama }}</h5>
-                        </div> --}}
-                        <div class="row">
-                            <div class="col-3">
-                                <img src="../../img/avatar-petugas.png" class="mx-auto d-block img-fluid" alt="" style="weight:200px; height:200px; border-radius:100px">
-                            </div>
-                            <div class="col-8">
-                                <form action="{{ url('petugas') }}" method="post">
-                                    @csrf
-                                    <div class="form-outline mb-3">
-                                        <label for="id">ID Petugas</label>
-                                        <input type="text" class="form-control" id="id" value="{{ Session::get('id') }}">
-                                    </div>
-                                    <div class="form-outline mb-3">
-                                        <label for="nama">Nama Petugas</label>
-                                        <input type="text" class="form-control" id="nama" value="{{ Session::get('nama') }}">
-                                    </div>
-                                    <div class="form-outline mb-3">
-                                        <label for="username">Username</label>
-                                        <input type="text" class="form-control" id="username" value="{{ Session::get('username') }}">
-                                    </div>
-                                    <div class="form-outline mb-3">
-                                        <label for="password">Password</label>
-                                        <input type="password" class="form-control" id="password">
-                                    </div>
-                                    <div class="form-outline mb-3">
-                                        <label for="level">Level</label>
-                                        <select class="form-select" id="level">
-                                            <option selected></option>
-                                            <option value="Administrator">Administrator</option>
-                                            <option value="Petugas">Petugas</option>
-                                        </select>
-                                    </div>
-                                    <div>
-                                        <a href="{{ url('petugas') }}" type="button" class="btn btn-outline"><i class="bi bi-arrow-left"></i> Kembali</a>
-                                        <button type="submit" class="btn text-white" style="background-color:#055E68"><i class="bi bi-box-arrow-in-down"></i> Simpan</button>
-                                    </div>
-                                </form>
-                            </div>
+    <main id="main" class="main">
+        <div class="pagetitle">
+            <h1>Form Validation</h1>
+            <nav>
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                    <li class="breadcrumb-item">Forms</li>
+                    <li class="breadcrumb-item active">Validation</li>
+                </ol>
+            </nav>
+        </div>
+        <section class="section">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Custom Styled Validation</h5>
+                            <p>For custom Bootstrap form validation messages, you’ll need to add the <code>novalidate</code> boolean attribute to your <code>&lt;form&gt;</code>. This disables the browser default feedback tooltips, but still provides access to the form validation APIs in JavaScript.</p>
+                            <form class="row g-3 needs-validation" novalidate>
+                                <div class="form-outline">
+                                    <label for="id" class="form-label">ID Petugas</label> 
+                                    <input type="text" class="form-control" id="id" value="" required>
+                                    <div class="valid-feedback"> Looks good!</div>
+                                </div>
+                                <div class="form-outline">
+                                    <label for="nama" class="form-label">Nama Petugas</label> 
+                                    <input type="text" class="form-control" id="nama" value="" required>
+                                    <div class="valid-feedback"> Looks good!</div>
+                                </div>
+                                <div class="form-outline">
+                                    <label for="username" class="form-label">Username</label>
+                                    <input type="text" class="form-control" id="username" aria-describedby="inputGroupPrepend" required>
+                                    <div class="invalid-feedback"> Please choose a username.</div>
+                                </div>
+                                <div class="form-outline">
+                                    <label for="password" class="form-label">Password</label> 
+                                    <input type="text" class="form-control" id="password" required>
+                                    <div class="invalid-feedback"> Please provide a valid city.</div>
+                                </div>
+                                <div class="form-outline">
+                                    <label for="level" class="form-label">Level</label> 
+                                    <select class="form-select" id="level" required>
+                                        <option selected disabled value="">Choose...</option>
+                                        <option>...</option>
+                                    </select>
+                                    <div class="invalid-feedback"> Please select a valid state.</div>
+                                </div>
+                                <div> 
+                                    <button class="btn btn-primary" type="submit">Submit form</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-
+        </section>
+    </main>
 @endsection
---
+++ resources/views/petugas/old file/edit.blade.php
@@ -0,0 +1,56 @@
+@extends('layout.main')
+
+@section('content')
+    <div class="container mb-4">
+        <h2 class="mt-3">Edit Data Petugas</h2>
+        <div class="row">
+            <div class="col">
+                <div class="card mt-3 mb-3">
+                    <div class="card-body">
+                        <div class="card-header mb-3" style="background-color:#055E68; max-height:60px">
+                            <h5 class="card-title mt-2 fw-medium text-light">{{ $petugas->nama }}</h5>
+                        </div>
+                        <div class="row">
+                            <div class="col-3">
+                                <img src="../../img/person-circle.svg" class="rounded mx-auto d-block img-fluid" alt="" style="weight:200px; height:200px;">
+                            </div>
+                            <div class="col-9">
+                                <form action="{{ url('petugas/'.$petugas->id) }}" method="">
+                                    @csrf
+                                    <div class="form-outline mb-3">
+                                        <label for="id">ID Petugas</label>
+                                        <input type="text" class="form-control" id="id" value="{{ $petugas->id }}" readonly>
+                                    </div>
+                                    <div class="form-outline mb-3">
+                                        <label for="nama">Nama Petugas</label>
+                                        <input type="text" class="form-control" id="nama" value="{{ $petugas->nama }}">
+                                    </div>
+                                    <div class="form-outline mb-3">
+                                        <label for="username">Username</label>
+                                        <input type="text" class="form-control" id="username" value="{{ $petugas->username }}">
+                                    </div>
+                                    <div class="form-outline mb-3">
+                                        <label for="password">Password</label>
+                                        <input type="password" class="form-control" id="password">
+                                    </div>
+                                    <div class="form-outline mb-3">
+                                        <label for="level">Level</label>
+                                        <select class="form-select" id="level">
+                                            <option selected></option>
+                                            <option value="Administrator">Administrator</option>
+                                            <option value="Petugas">Petugas</option>
+                                        </select>
+                                    </div>
+                                    <div>
+                                        <a href="{{ url('petugas') }}" type="button" class="btn btn-outline"><i class="bi bi-arrow-left"></i> Kembali</a>
+                                        <button type="submit" class="btn text-white" style="background-color:#055E68"><i class="bi bi-box-arrow-in-down"></i> Simpan</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
