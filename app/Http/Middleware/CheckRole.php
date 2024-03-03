fc57a272fdea5b062531c76222d69cd8e7c23a7e
r-aozora <zytrahidayat11@gmail.com>
2024-03-03 12:50:45 +0700

    feat: middleware CheckRole

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, ...$role): Response
+    {
+        if(in_array(Auth::user()->role, $role)){
+            return $next($request);
+        }
+
+        toast('Anda tidak punya akses!', 'warning');
+
+        return redirect()->back();
+    }
+}
